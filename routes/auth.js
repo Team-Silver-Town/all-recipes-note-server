dfe4caa9dbd82d5c92c6dd9e7964fa3dfbb7ea1f
Wonil Nam <apotatos11@gmail.com>
2022-06-09 01:20:56 +0900

    feat: auth api adding patchUser

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const authController = require("./controllers/auth.controller");
 
 router.get("/", authController.findUser);
 router.post("/", authController.createUser);
+router.patch("/", authController.patchUser);
 
 module.exports = router;
