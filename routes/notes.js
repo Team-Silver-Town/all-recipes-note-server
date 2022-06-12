0fdeaf311358049c004d2e3852408b813c410896
Scott <sp863429@gmail.com>
2022-06-12 20:04:52 +0900

    feat: notes and tips delete and update functionality
    노트와 꿀팁 수정 및 삭제 기능에 해당하는 crud 작업 구현

--- routes/notes.js
+++ routes/notes.js
@@ -5,11 +5,12 @@ const noteController = require("./controllers/note.controller");
 router.get("/", noteController.getAllNotes);
 router.get("/top10", noteController.getTopTenNotes);
 router.get("/:user_id", noteController.getNotesByUserId);
-router.post("/", noteController.createNote);
 router.get("/:note_id", noteController.getNote);
 router.get("/:user_id", noteController.getNotesByUserId);
+router.post("/", noteController.createNote);
 router.patch("/:note_id", noteController.updateNote);
 router.patch("/:note_id/likes", noteController.updateNoteLike);
 router.patch("/:note_id/unlikes", noteController.cancelNoteLike);
+router.delete("/:note_id", noteController.deleteNote);
 
 module.exports = router;
