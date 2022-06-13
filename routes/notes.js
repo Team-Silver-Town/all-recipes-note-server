cf7dfb3c1dc8d5c1d94bef65b7b358f490a3d133
Wonil Nam <apotatos11@gmail.com>
2022-06-13 17:08:42 +0900

    fix: delete duplicate phrase

--- routes/notes.js
+++ routes/notes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const noteController = require("./controllers/note.controller");
 
 router.get("/", noteController.getAllNotes);
+router.post("/", noteController.createNote);
 router.get("/top10", noteController.getTopTenNotes);
 router.get("/:user_id", noteController.getNotesByUserId);
 router.get("/:note_id", noteController.getNote);
-router.get("/:user_id", noteController.getNotesByUserId);
-router.post("/", noteController.createNote);
 router.patch("/:note_id", noteController.updateNote);
+router.delete("/:note_id", noteController.deleteNote);
 router.patch("/:note_id/likes", noteController.updateNoteLike);
 router.patch("/:note_id/unlikes", noteController.cancelNoteLike);
-router.delete("/:note_id", noteController.deleteNote);
 
 module.exports = router;
